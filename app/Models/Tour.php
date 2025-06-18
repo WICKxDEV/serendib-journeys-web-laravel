22d4d05dda6ed30b2dd80063c5235b3945a79d38
WICKxDEV <isuruwickramasinghe.slaite@gmail.com>
2025-06-18 10:46:59 +0530

    Models with Relationships

---
+++ app/Models/Tour.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tour extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['destination_id', 'title', 'description', 'price', 'itinerary', 'available_from', 'available_to'];
+
+    public function destination()
+    {
+        return $this->belongsTo(Destination::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+}
+
